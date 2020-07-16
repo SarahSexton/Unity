f799265c4a9d9e8714f621ae588c1b28f58c44cc
Sarah <sarahjeannesexton@live.com>
2020-07-16 03:34:32 -0700

    Update for Selecting Functions in the Editor

--- Basics/MathematicalSurfaces/Mathematical Surfaces/Assets/Graph.cs
+++ Basics/MathematicalSurfaces/Mathematical Surfaces/Assets/Graph.cs
@@ -1,37 +1,66 @@
 ﻿using UnityEngine;
 
-public class Graph : MonoBehaviour {
+public class Graph : MonoBehaviour
+{
 
-	public Transform pointPrefab;
+    public Transform pointPrefab;
 
-	[Range(10, 100)]
-	public int resolution = 10;
+    [Range(10, 100)]
+    public int resolution = 10;
 
-	Transform[] points;
+    [Range(0, 1)]
+    public int function;
 
-	void Awake () {
-		float step = 2f / resolution;
-		Vector3 scale = Vector3.one * step;
-		Vector3 position;
-		position.y = 0f;
-		position.z = 0f;
-		points = new Transform[resolution];
-		for (int i = 0; i < points.Length; i++) {
-			Transform point = Instantiate(pointPrefab);
-			position.x = (i + 0.5f) * step - 1f;
-			point.localPosition = position;
-			point.localScale = scale;
-			point.SetParent(transform, false);
-			points[i] = point;
-		}
-	}
+    Transform[] points;
 
-	void Update () {
-		for (int i = 0; i < points.Length; i++) {
-			Transform point = points[i];
-			Vector3 position = point.localPosition;
-			position.y = Mathf.Sin(Mathf.PI * (position.x + Time.time));
-			point.localPosition = position;
-		}
-	}
+    void Awake()
+    {
+        float step = 2f / resolution;
+        Vector3 scale = Vector3.one * step;
+        Vector3 position;
+        position.y = 0f;
+        position.z = 0f;
+        points = new Transform[resolution];
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+            point.localPosition = position;
+            point.localScale = scale;
+            point.SetParent(transform, false);
+            points[i] = point;
+        }
+    }
+
+    void Update()
+    {
+        float t = Time.time;
+        for (int i = 0; i < points.Length; i++)
+        {
+            Transform point = points[i];
+            Vector3 position = point.localPosition;
+            if (function == 0)
+            {
+                position.y = SineFunction(position.x, t);
+            }
+            else
+            {
+                position.y = MultiSineFunction(position.x, t);
+            }
+            point.localPosition = position;
+        }
+    }
+
+    float SineFunction(float x, float t)
+    {
+        return Mathf.Sin(Mathf.PI * (x + t));
+    }
+
+    float MultiSineFunction(float x, float t)
+    {
+        float y = Mathf.Sin(Mathf.PI * (x + t));
+        y += Mathf.Sin(2f * Mathf.PI * (x + 2f * t)) / 2f;
+        y *= 2f / 3f;
+        return y;
+    }
 }
